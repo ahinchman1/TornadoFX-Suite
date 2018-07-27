783ed2942141ba75b11813899cf2785ce83ff7e9
Amanda Hinchman-Dominguez <hinchman17@grinnell.edu>
2018-07-27 00:29:35 -0500

    simple layout

--- src/main/kotlin/com/github/hd/tornadofxsuite/app/Styles.kt
+++ src/main/kotlin/com/github/hd/tornadofxsuite/app/Styles.kt
@@ -1,14 +1,14 @@
 package com.github.hd.tornadofxsuite.app
 
+import javafx.scene.paint.Color
 import javafx.scene.text.FontWeight
-import tornadofx.Stylesheet
-import tornadofx.box
-import tornadofx.cssclass
-import tornadofx.px
+import tornadofx.*
 
 class Styles : Stylesheet() {
     companion object {
         val heading by cssclass()
+        val mainScreen by cssclass()
+        val top by cssclass()
     }
 
     init {
@@ -16,6 +16,15 @@ class Styles : Stylesheet() {
             padding = box(10.px)
             fontSize = 20.px
             fontWeight = FontWeight.BOLD
+        }
+
+        mainScreen {
+            backgroundColor += c("#222")
+        }
+
+        top {
+            backgroundColor += Color.PERU
+            padding = box(20.px)
         }
     }
 }
--- src/main/kotlin/com/github/hd/tornadofxsuite/view/MainView.kt
+++ src/main/kotlin/com/github/hd/tornadofxsuite/view/MainView.kt
@@ -1,12 +1,31 @@
 package com.github.hd.tornadofxsuite.view
 
 import com.github.hd.tornadofxsuite.app.Styles
+import javafx.application.Platform
 import tornadofx.*
+import kotlin.concurrent.thread
 
-class MainView : View("Hello TornadoFX") {
-    override val root = hbox {
-        label(title) {
-            addClass(Styles.heading)
+class MainView : View("TornadoFX-Suite") {
+    override val root = vbox {
+        prefWidth = 400.0
+        prefHeight = 600.0
+
+        hbox {
+            imageview("tornado-fx-logo.png")
+            hboxConstraints {
+                marginLeftRight(20.0)
+                marginTopBottom(20.0)
+            }
+        }.addClass(Styles.top)
+
+        progressindicator {
+            thread {
+                for (i in 1..100) {
+                    Platform.runLater { progress = i.toDouble() / 100.0 }
+                    Thread.sleep(100)
+                }
+            }
         }
-    }
+
+    }.addClass(Styles.mainScreen)
 }
