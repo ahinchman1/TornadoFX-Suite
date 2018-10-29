dcb3a499025c93f386d1fff5445c9453b2bfe942
Matt Moore <mattmoore@carbonhelix.com>
2018-10-29 05:15:08 -0500

    Fix file loading for logo
    - Looks like loading a file resource requires prepending "file:" in
    front of the path.

--- src/main/kotlin/com/github/hd/tornadofxsuite/view/MainView.kt
+++ src/main/kotlin/com/github/hd/tornadofxsuite/view/MainView.kt
@@ -23,7 +23,7 @@ class MainView : View() {
             prefHeight = 600.0
 
             hbox {
-                imageview("tornado-fx-logo.png")
+                imageview("file:tornado-fx-logo.png")
                 hboxConstraints {
                     marginLeftRight(20.0)
                     marginTopBottom(20.0)
